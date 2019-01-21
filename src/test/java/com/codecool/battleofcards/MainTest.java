87ade124aaaed942cf18880f7afd57b20176a5db
Liz Ch <pachaffy@gmail.com>
2019-01-21 11:30:27 +0100

    Correct class name in MainTest

--- src/test/java/com/codecool/battleofcards/MainTest.java
+++ src/test/java/com/codecool/battleofcards/MainTest.java
@@ -7,7 +7,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class Main 
+public class MainTest
     extends TestCase
 {
     /**
@@ -15,7 +15,7 @@ public class Main
      *
      * @param testName name of the test case
      */
-    public Main( String testName )
+    public MainTest( String testName )
     {
         super( testName );
     }
@@ -25,7 +25,7 @@ public class Main
      */
     public static Test suite()
     {
-        return new TestSuite( Main.class );
+        return new TestSuite( MainTest.class );
     }
 
     /**
