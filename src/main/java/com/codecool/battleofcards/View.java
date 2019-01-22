d28dc5f69c6152b10a971eb68fcf453ce9e9c57b
Mateusz Steliga <mateusz.steliga@codecool.com>
2019-01-22 14:12:44 +0100

    View class implementation

--- src/main/java/com/codecool/battleofcards/View.java
+++ src/main/java/com/codecool/battleofcards/View.java
@@ -4,21 +4,53 @@ import java.util.Scanner;
 
 public class View {
 
-    private Scanner scanner;
+    public void displayCard (String name, int attack, int defence, int HP, int inteligence) {
+        String leftAlignFormat =
+                "+====+=====================+\n"+
+                        "|@@@@|                     | \n"+
+                        "|@@@@| name : %-5s        | \n"+ "+====+=====================+\n"+
+                        "|####| attack      : %-5s | \n"+
+                        "|####|                     | \n"+
+                        "|####| defence     : %-5s | \n"+
+                        "|####|                     | \n"+
+                        "|####| HP          : %-5s | \n"+
+                        "|####|                     | \n"+
+                        "|####| inteligence : %-5s | \n"+
+                        "|####|                     | \n"+
+                        "+====+=====================+%n";
+        System.out.format(leftAlignFormat, name ,attack, defence, HP, inteligence);
 
-    public String displayCard() {
 
     }
 
-    public String getUserInput() {
+    public int getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
 
     }
 
-    public String displayMenu() {
+    public void displayMenu() {
+        System.out.println("                              `                  0\n" +
+                "  .:XHHHHk.              db.   .;;.     dH  MX   0\n" +
+                "oMMMMMMMMMMM       ~MM  dMMP :MMMMMR   MMM  MR      ~MRMN\n" +
+                "QMMMMMb  \"MMX       MMMMMMP !MX' :M~   MMM MMM  .oo. XMMM 'MMM\n" +
+                "  `MMMM.  )M> :X!Hk. MMMM   XMM.o\"  .  MMMMMMM X?XMMM MMM>!MMP\n" +
+                "   'MMMb.dM! XM M'?M MMMMMX.`MMMMMMMM~ MM MMM XM `\" MX MMXXMM\n" +
+                "    ~MMMMM~ XMM. .XM XM`\"MMMb.~*?**~ .MMX M t MMbooMM XMMMMMP\n" +
+                "     ?MMM>  YMMMMMM! MM   `?MMRb.    `\"\"\"   !L\"MMMMM XM IMMM\n" +
+                "      MMMX   \"MMMM\"  MM       ~%:           !Mh.\"\"\" dMI IMMP\n" +
+                "      'MMM.                                             IMX\n" +
+                "       ~M!M                                             IMP");
+
+        System.out.println("Welcome to Pokemon cards game!");
+        System.out.println( "1. Start game\n"+
+                            "2. Show authors\n"+
+                            "3. Quit game\n");
 
     }
 
-    public String printText() {
+    public String printText(String yourText) {
+        return yourText.toString();
 
     }
 }
