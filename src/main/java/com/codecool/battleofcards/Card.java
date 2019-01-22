e687f8c5d0c9f82025c6878455beef41eabb6c0e
Tatiana Slonimskaia <slonimskaia@gmail.com>
2019-01-22 11:20:30 +0100

    Implement of Card class, Implement Dao class

--- src/main/java/com/codecool/battleofcards/Card.java
+++ src/main/java/com/codecool/battleofcards/Card.java
@@ -1,3 +1,41 @@
 public class Card {
-    
+    private String id;
+    private String name;
+    private int attack;
+    private int defense;
+    private int hp;
+    private int speed;
+
+    public Card(String id, String name, int attack, int defense, int hp, int speed) {
+        this.id = id;
+        this.name = name;
+        this.attack = attack;
+        this.defense = defense;
+        this.hp = hp;
+        this.speed = speed;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getdefense() {
+        return defense;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
 }
---
+++ src/main/java/com/codecool/battleofcards/Dao.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Dao {
+    private List<Card> cards;
+
+    public Dao() {
+        cards = new ArrayList<Card>();
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void readCSV() {
+
+    }
+
+    public void addCards() {
+        
+    }
+}
