fb6874d684d5819ce9fa5a99b5599700b21cb85e
Wojciech Charuza <wojciechcharuza@MacBook-Pro-Wojciech-3.local>
2019-01-22 13:28:59 +0100

    DefenseComparator created

---
+++ src/main/java/com/codecool/battleofcards/DefenseComparator.java
@@ -0,0 +1,11 @@
+package com.codecool.battleofcards;
+
+import java.util.Comparator;
+
+public class DefenseComparator implements Comparator<Card> {
+
+    @Override
+    public int compare(Card playerOneCard, Card playerTwoCard) {
+        return (playerOneCard.getdefense() - playerTwoCard.getdefense());
+    }
+}
