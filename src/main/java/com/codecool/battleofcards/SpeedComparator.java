b281d5180a07bc6c3f1aa07047ee6b3db98fa81b
Wojciech Charuza <wojciechcharuza@MacBook-Pro-Wojciech-3.local>
2019-01-22 13:30:40 +0100

    SpeedComparator created

---
+++ src/main/java/com/codecool/battleofcards/SpeedComparator.java
@@ -0,0 +1,11 @@
+package com.codecool.battleofcards;
+
+import java.util.Comparator;
+
+public class SpeedComparator implements Comparator<Card> {
+
+    @Override
+    public int compare(Card playerOneCard, Card playerTwoCard) {
+        return (playerOneCard.getSpeed() - playerTwoCard.getSpeed());
+    }
+}
