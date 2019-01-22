f1ffc65a947a1fd58ed088db4579fbb2ef9c4009
Wojciech Charuza <wojciechcharuza@MacBook-Pro-Wojciech-3.local>
2019-01-22 13:01:02 +0100

    deck and dealer methods implemented

--- src/main/java/com/codecool/battleofcards/Dealer.java
+++ src/main/java/com/codecool/battleofcards/Dealer.java
@@ -1,8 +1,13 @@
 package com.codecool.battleofcards;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Dealer {
 
-    private Table table;
+    private Deck deck;
+    private Dao dao;
 
     public Dealer() {
 
@@ -10,5 +15,13 @@ public class Dealer {
 
     public void deal() {
 
+    }
+
+    public void shuffleCards() {
+        Collections.shuffle(deck.getCards());
+    }
+
+    public void addCards() {
+        deck.setCards(dao.getCards());
     }
 }
--- src/main/java/com/codecool/battleofcards/Deck.java
+++ src/main/java/com/codecool/battleofcards/Deck.java
@@ -1,24 +1,21 @@
 package com.codecool.battleofcards;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Deck {
 
     private List<Card> cards;
 
     public Deck() {
-
-    }
-
-    public void shuffleCards() {
-
-    }
-
-    public void addCards() {
-
+        cards = new ArrayList<Card>();
     }
 
     public List<Card> getCards() {
+        return cards;
+    }
 
+    public void setCards(List<Card> cardsToSet) {
+        this.cards = cardsToSet;
     }
 }
